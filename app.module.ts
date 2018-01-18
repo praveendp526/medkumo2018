f61c5ffaa826eccdf9a92e489fe1a452f3fce5e6
praveendp526 <praveendp526@yahoo.com>
2018-01-18 19:22:25 +0530

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { Contact } from 'app/contact';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactsComponent,Contact
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
